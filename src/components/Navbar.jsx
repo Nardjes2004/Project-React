1c77cdb9afcbc855012a71e4f199a603800857e5
Nardjes2004 <nardjesanane@gmail.com>
2024-11-06 18:35:09 +0100

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <header className='py-4 border-b border-b-black'>
-      <nav className='flex justify-between items-center max-w-screen-lg mx-auto'>
-        <img className='w-16' src='/Logo.png' alt=''/>
-        <ul className='flex gap-10 '>
-            <li className='text-lg font-semibold hover:underline hover:cursor-pointer'>Home</li>
-            <li className='text-lg font-semibold hover:underline hover:cursor-pointer'>Checkout</li>
-            <li className='text-lg font-semibold hover:underline hover:cursor-pointer'>Shop</li>
+<header className="sticky top-0 bg-white shadow-[0_4px_6px_rgba(0,0,0,0.4)] py-2">
+    <nav className="flex justify-between items-center max-w-screen-lg mx-auto">
+        <img 
+            className="w-16 rounded-lg grayscale transition ease-in-out duration-300 hover:scale-110 hover:grayscale-0" 
+            src="/Logo.png" 
+            alt="Logo"
+        />
+        <ul className="flex gap-10 list-none cursor-pointer">
+            <li className="text-lg font-semibold hover:underline ">Home</li>
+            <li className="text-lg font-semibold hover:underline ">Checkout</li>
+            <li className="text-lg font-semibold hover:underline ">Shop</li>
         </ul>
+    </nav>
+</header>
 
-
-      </nav>
-    </header>
   )
 }
