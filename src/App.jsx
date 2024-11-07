506cb118fd8e9ff7e95afa43366f6e850083b38c
Nardjes2004 <nardjesanane@gmail.com>
2024-11-07 16:58:57 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
   return (
     <>
-      <Navbar />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={Layout} >
+          <Route path="/shop" element={Products} />
+             <Route path="/shop/:id" element={ProductSingle} />
+             <Route path="/chechout/:ids" element={Checkout} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
